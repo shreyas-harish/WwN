7ae8cae6f7ab9ba03b61084455fda37621119bff
Shreyas Harish <shreyasharish31@gmail.com>
2021-06-28 03:39:52 +0530

    Reads input.txt but doesn't do anything with input

--- readInput.py
+++ readInput.py
@@ -1,2 +1,7 @@
 #This file contains helper functions to read txt files and store the input in the form of a network/graph.
-#Test edit
+from graphDefs import *
+
+file1 = open('/Users/shreyasharish/Documents/WwN/input.txt','r')
+Lines = file1.readlines()
+for lin in Lines:
+    print(lin.strip())
