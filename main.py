e5b6897a77f5369fe1128467c3c9e39f3ce20e8e
Shreyas Harish <shreyasharish31@gmail.com>
2021-08-15 17:15:34 +0530

    cycle cancelling algorithm completed

--- main.py
+++ main.py
@@ -11,12 +11,36 @@ from maxflow import *
 from minCost import *
 
 g1 = inputFile("/Users/shreyasharish/Documents/WwN/input2.txt")
-flow = pushRelabelHeuristics(g1,"0","7")
-print(flow["flow"])
-flow["flowGraph"].printGraph()
-minCostFlow = capacityScaling(g1,"0","7",4)
-print(minCostFlow["cost"])
-minCostFlow["flowGraph"].printGraph()
+flow = pushRelabelHeuristics(g1,"7","29")
+#print(flow["flow"])
+#flow["flowGraph"].printGraph()
+#minCostFlow2 = cycleCancelling(g1,"0","39",98)
+#print(minCostFlow2["cost"])
+
+i = 1
+matchCount = 0
+missCount = 0
+while i <= flow["flow"]:
+    minCostFlow1 = capacityScaling(g1,"7","29",i)
+    minCostFlow2 = cycleCancelling(g1,"7","29",i)
+    if minCostFlow1["cost"] == minCostFlow2["cost"]:
+        matchCount += 1
+        #print("flow volume ",i," -> cost ",minCostFlow1["cost"])
+    else:
+        missCount += 1
+        print("flow volume ",i," -> cost1 ",minCostFlow1["cost"]," cost2 ",minCostFlow2["cost"])
+        #print("Graph 1")
+        #minCostFlow1["flowGraph"].printGraph()
+        #print("Graph 2")
+        #minCostFlow2["flowGraph"].printGraph()
+    i += 1
+
+
+#print(minCostFlow["cost"])
+print("matches ",matchCount)
+print("misses ",missCount)
+#print(minCostFlow["cost"])
+#minCostFlow["flowGraph"].printGraph()
 """
 flow = pushRelabel(g1,"0","5")
 print(flow["flow"])
