38d2e7daa5e95b442369eba67fbc15954755716d
Shreyas Harish <shreyasharish31@gmail.com>
2021-08-28 14:04:06 +0530

    pseudocode for input set to graph in place

--- olympicEventObjects.py
+++ olympicEventObjects.py
@@ -1,3 +1,4 @@
+from graphDefs import *
 # This file contains definitions for an olympic event and sport
 
 # Definition of a sport
@@ -39,6 +40,8 @@ class event:
         self.venue = venue
         self.startTime = startTime
         self.endTime = endTime
+        self.startingNodeID = "event start not set"
+        self.endingNodeID = "event end not set"
 
     # Function to set or change starting node ID
     def setStartingNodeID(self, startingNodeID):
@@ -48,6 +51,45 @@ class event:
     def setEndingNodeID(self, endingNodeID):
         self.endingNodeID = endingNodeID
 
+    # Function to print an event object and its values
+    def printEvent(self):
+        print("sport name -> ", self.sport, end=" | ")
+        print("venue -> ", self.venue, end=" | ")
+        print("start time -> ", self.startTime, end=" | ")
+        print("end time -> ", self.endTime)
+
 # Useful functions to run on groups of event objects
 
-# TODO: Function to print a dict of event objects
+# Function to print a list of event objects
+
+
+def printListOfEvents(listOfEvents):
+    for event in listOfEvents:
+        event.printEvents()
+
+#Function to setup start and end nodes of graph, with required reporter constraint
+def setupStartAndEnd(reporterLimit):
+    outputGraph = graph()
+    outputGraph.addNode("start")
+    outputGraph.addNode("start capacity constrained")
+    outputGraph.addNode("end")
+    capacity = 1000
+    if not reporterLimit == None:
+        capacity = reporterLimit
+    outputGraph.addEdge("start","start capacity constrained",capacity)
+
+    return outputGraph
+
+#Function to accept an input set and return a graph modelling the scheduling problem and constraints
+def inputToGraph(inputSet):
+    #Setup graph and add two start nodes, with joining edge which limits capcity to 1000 or #reporters and one end node
+    outputGraph = setupStartAndEnd(inputSet["reporterLimit"])
+
+    #TODO: iterate through all event objects and add nodes + edges appropriately
+        #TODO: Add a start node and an end node "event <number> start/end"
+        #TODO: Add node ids to the event object
+        #TODO: Add an edge with appropriate capacity and cost
+        #TODO: Add an edge from source, with 1000 capcity, no cost + add an edge to sink with 1000 capacity, no cost
+        #TODO: Write a function to iterate through existing nodes, find matches and add appropriate edges
+
+    #TODO: Return the final graph and the input set (updated with node IDs in events)
